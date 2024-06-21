b6a2cbe91ed94e234cf021aa637e644712d31228
git-cameronbryce <developmentcameronbryce@gmail.com>
2024-06-21 11:02:50 -0600

    chore: add ESLint configuration enforce semicolon

--- source/script.ts
+++ source/script.ts
@@ -1,6 +1,6 @@
-import { discord, database } from './other-config/configuration.json'
+import { discord, database } from './other-config/configuration.json';
 import { Client, GatewayIntentBits } from 'discord.js';
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 import { CommandKit } from 'commandkit';
 
 import path from 'path';
@@ -8,17 +8,16 @@ const client: Client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
 
-const supabaseUrl = database.url
-const supabaseKey = database.key
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = database.url;
+const supabaseKey = database.key;
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 new CommandKit({
   client,
   commandsPath: path.join(__dirname, 'command-handler'),
   eventsPath: path.join(__dirname, 'event-handler'),
   devGuildIds: ['1253089110026096752'],
-  bulkRegister: true
+  bulkRegister: true,
 });
 
-client.login(discord.token)
-  .then(() => console.log('Client logged in...'))
+client.login(discord.token).then(() => console.log('Client logged in...'));
